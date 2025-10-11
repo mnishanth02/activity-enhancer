f6c8195d4179d9b7d93810609a1400ac808e6b62
Nishanth Murugan <nishanth.murugan@gmail.com>
2025-10-11 17:50:45 +0530

    feat: add UI components including Skeleton, Slider, Sonner, Spinner, Switch, Table, Tabs, Textarea, Toggle Group, Toggle, Tooltip, and custom hook for mobile detection

--- docs/youtube/Steps.md
+++ docs/youtube/Steps.md
@@ -6,5 +6,6 @@
 
 - intialize the fresh project from wxt website
 - configure the tailwind css
+- Configure biome
 - confiure shandcn components
 -
--- src/entrypoints/popup/main.tsx
+++ src/entrypoints/popup/main.tsx
@@ -3,9 +3,10 @@ import ReactDOM from "react-dom/client";
 import "@/assets/style.css";
 import { Button } from "@/components/ui/button";
 
+// biome-ignore lint/style/noNonNullAssertion: <false positive>
 ReactDOM.createRoot(document.getElementById("root")!).render(
 	<React.StrictMode>
-		<div className="">
+		<div >
 			<h1 className="text-3xl font-bold underline">Hello world!</h1>
 			<Button>Button</Button>
 		</div>

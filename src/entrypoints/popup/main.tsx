8928a027977574775824475954585a55fceaeaf5
Nishanth Murugan <nishanth.murugan@gmail.com>
2025-10-11 16:49:01 +0530

    feat: add biome configuration and update package dependencies

--- src/entrypoints/popup/main.tsx
+++ src/entrypoints/popup/main.tsx
@@ -1,13 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import '@/assets/style.css';
-import { Button } from '@/components/ui/button';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "@/assets/style.css";
+import { Button } from "@/components/ui/button";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <div className="">
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
-      <Button >Button</Button>
-    </div>
-  </React.StrictMode>,
+ReactDOM.createRoot(document.getElementById("root")!).render(
+	<React.StrictMode>
+		<div className="">
+			<h1 className="text-3xl font-bold underline">Hello world!</h1>
+			<Button>Button</Button>
+		</div>
+	</React.StrictMode>,
 );
